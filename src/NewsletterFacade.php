3a48201a2ce9488f6b9e407bdd2bbb2111448322
Aaron Ho <616127+cincauhangus@users.noreply.github.com>
2020-03-17 15:28:19 +0800

    phpDoc for NewsletterFacade's internal functions

--- src/NewsletterFacade.php
+++ src/NewsletterFacade.php
@@ -4,6 +4,25 @@ namespace Spatie\Newsletter;
 
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * @method static array|false subscribe(string $email, array $mergeFields = [], string $listName = '', array $options = [])
+ * @method static array|false subscribePending(string $email, array $mergeFields = [], string $listName = '', array $options = [])
+ * @method static array|false subscribeOrUpdate(string $email, array $mergeFields = [], string $listName = '', array $options = [])
+ * @method static array|false getMembers(string $listName = '', array $parameters = [])
+ * @method static array|false getMember(string $email, string $listName = '')
+ * @method static array|false getMemberActivity(string $email, string $listName = '')
+ * @method static array|false hasMember(string $email, string $listName = ''): bool
+ * @method static array|false isSubscribed(string $email, string $listName = ''): bool
+ * @method static array|false unsubscribe(string $email, string $listName = '')
+ * @method static array|false updateEmailAddress(string $currentEmailAddress, string $newEmailAddress, string $listName = '')
+ * @method static array|false delete(string $email, string $listName = '')
+ * @method static array|false createCampaign(
+ * @method static array|false updateContent(string $campaignId, string $html, array $options = [])
+ * @method static array|false getLastError()
+ * @method static DrewM\MailChimp\MailChimp getApi()
+ * @method static bool lastActionSucceeded()
+ */
+
 class NewsletterFacade extends Facade
 {
     public static function getFacadeAccessor()
